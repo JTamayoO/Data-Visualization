0a17be143ee0f5000fa5cb656f1b5a1ab3d27fa7
Jonathan Tamayo <jtamayo@hugeinc.com>
2018-06-04 15:00:37 -0500

    JSON

--- Google Maps/logic.js
+++ Google Maps/logic.js
@@ -6,36 +6,8 @@ function initMap() {
   });
 
   // Load GeoJSON.
-  map.data.loadGeoJson('https://raw.githubusercontent.com/JTamayoO/Test/master/D3JS.js/data.json');
+  map.data.loadGeoJson('https://raw.githubusercontent.com/JTamayoO/Test/master/Google%20Maps/UAn_DensUrbam_28052018.geojson');
 
   // Color each letter gray. Change the color when the isColorful property
   // is set to true.
-  map.data.setStyle(function(feature) {
-    var color = 'gray';
-    if (feature.getProperty('isColorful')) {
-      color = feature.getProperty('color');
-    }
-    return /** @type {google.maps.Data.StyleOptions} */({
-      fillColor: color,
-      strokeColor: color,
-      strokeWeight: 2
-    });
-  });
-
-  // When the user clicks, set 'isColorful', changing the color of the letters.
-  map.data.addListener('click', function(event) {
-    event.feature.setProperty('isColorful', true);
-  });
-
-  // When the user hovers, tempt them to click by outlining the letters.
-  // Call revertStyle() to remove all overrides. This will use the style rules
-  // defined in the function passed to setStyle()
-  map.data.addListener('mouseover', function(event) {
-    map.data.revertStyle();
-    map.data.overrideStyle(event.feature, {strokeWeight: 8});
-  });
-
-  map.data.addListener('mouseout', function(event) {
-    map.data.revertStyle();
-  });
 }
